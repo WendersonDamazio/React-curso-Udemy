d98597b6f9fe6a7458773360f5bdee944157b805
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-04-18 11:59:13 -0300

    Create App.js

---
+++ secao5/eventos/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Membro from './componetes/Membro';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Membro/>
+      </div>
+    );
+  }
+}
+export default App;
