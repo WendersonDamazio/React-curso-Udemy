754f76822fa8ef80362fd90cf19710e256bd5bfe
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-05-11 11:29:06 -0300

    Create App.js

---
+++ secao6-hooks/useeffect/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  const[tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', )
+  }, [tarefas])
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+
+  return (
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      
+      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
