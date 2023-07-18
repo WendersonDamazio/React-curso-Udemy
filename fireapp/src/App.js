d8e3bb10f5ea6cc0870b349b23b91e084671671e
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-07-18 17:36:40 -0300

    Update App.js

--- fireapp/src/App.js
+++ fireapp/src/App.js
@@ -1,50 +1,91 @@
 import { useState } from 'react'
 import { db } from './firebaseconection'
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore' 
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
 
 import './app.css'
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
 
-  async function handleAdd(){
+  const [posts, setPosts] = useState([]);
+
+  async function handleAdd() {
     // await setDoc(doc(db, "posts", "12345"), {
-      // titulo: titulo,
-      // autor: autor,
+    // titulo: titulo,
+    // autor: autor,
     // })
     // .then(() => {
-      // console.log('Dados registrado no banco')
+    // console.log('Dados registrado no banco')
     // })
     // .catch((error) => {
-      // console.log('GEROU ERRO' + error)
+    // console.log('GEROU ERRO' + error)
     // })
     await addDoc(collection(db, "posts"), {
       titulo: titulo,
       autor: autor,
     })
-    .then(() => {
-      console.log("CADASTRADO COM SUCESSO")
-      setAutor('');
-      setTitulo('')
-    })
-    .catch((error) => {
-      console.log("ERROR " + error)
-    })
+      .then(() => {
+        console.log("CADASTRADO COM SUCESSO")
+        setAutor('');
+        setTitulo('')
+      })
+      .catch((error) => {
+        console.log("ERROR " + error)
+      })
 
   }
 
-  async function buscarPost(){
-    const postRef = doc(db, "posts", "Magi8eujRKU9AAVVyiJp")
+  async function buscarPost() {
+    //const postRef = doc(db, "posts", "RIaZnMrfURv6QP4tMeHH")
 
-    await getDoc(postRef)
-    .then((snapshat) => {
-      setAutor(snapshat.data().autor)
-      setTitulo(snapshat.data().titulo)
+    //await getDoc(postRef)
+    //.then((snapshat) => {
+    //setAutor(snapshat.data().autor)
+    //setTitulo(snapshat.data().titulo)
 
+    //})
+    //.catch(() => {
+    //console.log("Ero ao buscar")
+    //})
+
+    const postRef = collection(db, "posts")
+    await getDocs(postRef)
+      .then((snapshat) => {
+        let lista = [];
+
+        snapshat.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
+        })
+
+        setPosts(lista);
+
+      })
+      .catch((error) => {
+        console.log("DEU ALGUM ERRO")
+      })
+
+  }
+
+  async function editarPost(){
+    const docRef = doc(db, "posts", idPost)
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor,
     })
-    .catch(() => {
-      console.log("Ero ao buscar")
+    .then(() => {
+      console.log("POST ATUALIZADO")
+      setPosts('')
+      setTitulo('')
+      setAutor('')
+    })
+    .catch((error) =>{
+      console.log("ERRO AO ATUALIZAR")
     })
   }
 
@@ -53,25 +94,47 @@ function App() {
       <h1>ReactJS + FireBase :(</h1>
 
       <div className='container'>
+
+        <label>ID do Post: </label>
+        <input
+          placeholder='Digite o ID do post'
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        />
+
         <label>Titulo</label>
-        <textarea 
+        <textarea
           placeholder='Digite o titulo'
           type='text'
           value={titulo}
-          onChange={ (e)=> setTitulo(e.target.value) }
+          onChange={ (e) => setTitulo(e.target.value) }
         />
 
         <label>Autor:</label>
-        <input 
-          type='text' 
+        <input
           placeholder='autor do post'
           value={autor}
-          onChange={ (e)=> setAutor(e.target.value) }
-        />
+          onChange={(e) => setAutor(e.target.value)}
+        /><br/>
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPost}>Buscar post</button>
+        <button onClick={buscarPost}>Buscar post</button> <br/>
+
+        <button onClick={editarPost}>Atualizar post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return(
+              <li key={post.id}>
+                <strong>ID: {post.id}</strong> <br/>
+                <span>Titulo: {post.titulo}</span> <br/>
+                <span>Autor: {post.autor}</span><br/> <br></br>
+              </li>
+            )
+          })}
+        </ul>
       </div>
+
     </div>
   );
 }
